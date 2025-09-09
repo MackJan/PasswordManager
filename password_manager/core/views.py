b5595d78a4aae4ca1d3be16181cb8f7c3c773732
Jan Mack <42471731+MackJan@users.noreply.github.com>
2025-09-09 13:13:22 +0300

    Refactor HTML templates and styles for improved layout and user experience

--- password_manager/core/views.py
+++ password_manager/core/views.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import redirect
 
 def home(request):
     template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+    context = {
+        "authenticated": request.user.is_authenticated,
+    }
+    return HttpResponse(template.render(context,request))
+
+def root(request):
+    return redirect("/home/")
