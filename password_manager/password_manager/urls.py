eca197e69872128a52eea00cdf8cf3e9c2ee47be
Jan Mack <42471731+MackJan@users.noreply.github.com>
2025-09-09 19:05:59 +0300

    Refactor HTML structure and styles for login and registration pages; update entrypoint script to use bash

--- password_manager/password_manager/urls.py
+++ password_manager/password_manager/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +23,3 @@ urlpatterns = [
     path('',include('accounts.urls'))
 ]
 
-# Serve static files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
