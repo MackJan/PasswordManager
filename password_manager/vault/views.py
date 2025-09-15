ee52f7d7c92be602257b3e0e4d9ae6dc9bb68a6d
Jan Mack <42471731+MackJan@users.noreply.github.com>
2025-09-15 16:58:56 +0300

    Add a vault dashboard with a popup for item creation

--- password_manager/vault/views.py
+++ password_manager/vault/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, redirect
 
+from .models import *
+from django.contrib import messages
+
 # Create your views here.
 
 def vault_dashboard(request):
     if not request.user.is_authenticated:
         return redirect('/login')
-    return render(request, 'dashboard.html')
+    if request.method == "POST":
+        item_name = request.POST.get('name')
+        item_username = request.POST.get('username')
+        item_password = request.POST.get('password')
+
+        if VaultItem.objects.filter(name=item_name).exists():
+            messages.error(request, 'Item already exists')
+            return redirect('/vault/')
+
+        item = VaultItem(name=item_name, username=item_username, password=item_password,user_id=request.user.id)
+        if item is None:
+            messages.error(request, 'Item does not exist')
+            return redirect('/vault/')
+        else:
+            item.save()
+
+
+    context = {
+        "items": VaultItem.objects.all(),
+    }
+    return render(request, 'dashboard.html',context=context)
