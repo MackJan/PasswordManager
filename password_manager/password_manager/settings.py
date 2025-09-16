e00e3dbad17809a47650ecf2c284cce10701416e
Jan Mack <42471731+MackJan@users.noreply.github.com>
2025-09-16 12:46:29 +0300

    hotfix deploy action

--- password_manager/password_manager/settings.py
+++ password_manager/password_manager/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","localhost 127.0.0.1").split(" ")
-CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_ALLOWED_HOSTS","https://127.0.0.1").split(" ")
-CSRF_ALLOWED_ORIGINS = os.environ.get("DJANGO_ALLOWED_HOSTS","").split(" ")
-CORS_ORIGINS_WHITELIST = os.environ.get("DJANGO_ALLOWED_HOSTS","").split(" ")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS","https://127.0.0.1").split(" ")
+CSRF_ALLOWED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS","").split(" ")
+CORS_ORIGINS_WHITELIST = os.environ.get("CSRF_TRUSTED_ORIGINS","").split(" ")
 
 
 # Application definition
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 
 # Production static files configuration
 # Production static files configuration for WhiteNoise
-STATIC_ROOT = '/usr/src/app/staticfiles'
+STATIC_ROOT = '/var/www/static/'
 
 # Tell Django where to find static files in your apps
 STATICFILES_DIRS = [
