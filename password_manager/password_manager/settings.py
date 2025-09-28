613203c3825016807e7b2ace6be001bf19e5ee81
Jan Mack <42471731+MackJan@users.noreply.github.com>
2025-09-28 15:13:24 +0300

    debug hotfix

--- password_manager/password_manager/settings.py
+++ password_manager/password_manager/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = bool(os.environ.get("DEBUG", default=False))
 
 ALLOWED_HOSTS = [
     host.strip()
